9b6abd19ef2a0b279b639e8295606c13ee3ac94a
Anderson <gaabrieelbarbosaa@gmail.com>
2024-06-11 16:41:54 -0300

    190 mudei muita coisa, tabuleiro, peças adcionadas e etc

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,30 @@
 package application;
 
-import boardgame.Position;
+import java.util.Scanner;
+
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-				
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+		}
 	}
 
 }
