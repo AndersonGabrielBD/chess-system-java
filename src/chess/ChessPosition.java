c743e24d91862e5c49b08be3f226bab6ba4b1134
Anderson <gaabrieelbarbosaa@gmail.com>
2024-06-12 16:41:13 -0300

    projeto finalizado

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -6,32 +6,33 @@ public class ChessPosition {
 	
 	private char column;
 	private int row;
+	
 	public ChessPosition(char column, int row) {
-		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Erro estanciando chessPosition valores validos são de A1 ate H8");
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Erro ao instanciar posição de xadrez valores validos são de a1 a h8");
 		}
 		this.column = column;
 		this.row = row;
 	}
+
 	public char getColumn() {
 		return column;
 	}
-	
+
 	public int getRow() {
 		return row;
 	}
-	
+
 	protected Position toPosition() {
-		return new Position (8 - row, column - 'a');
-		
+		return new Position(8 - row, column - 'a');
 	}
+	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a'- position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
 	public String toString() {
 		return "" + column + row;
 	}
-
 }
